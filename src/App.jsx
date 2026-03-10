4c85533497cc0358bf24c7d8ef82fbc7bc7197b0
Scout <111934300+lilypad4321@users.noreply.github.com>
2026-03-10 16:13:39 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 function App() {
+  const [showForgot, setShowForgot] = useState(false);
+
   return (
     <div>
       <h1>Rowan Budget Companion</h1>
       <p>A student centric budgeting tool!</p>
+
+      {showForgot ? (
+        <ForgotPassword />
+      ) : (
+        <Login onForgotPassword={() => setShowForgot(true)} />
+      )}
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
